250b6085e0b4973f8359e70016a6945a3abf2cb2
scw0108 <bruce900108@icloud.com>
2021-09-03 10:31:35 +0800

    flask基礎網站開發

---
+++ 初學/flask/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app=Flask(__name__)    #__name__ 代表目前執行的模組
+
+@app.route("/")    #函式的裝飾(Decotator):以函式為基礎,提供附加功能
+def home():
+	return "Hello Flask"
+
+if __name__ =="__main__":    #如果以主程式執行
+	app.run()    #立刻啟動伺服器
