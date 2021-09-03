649e089947fecb100f22b411f0392741df4d6c45
scw0108 <bruce900108@icloud.com>
2021-09-03 10:55:32 +0800

    / 與 /test差異性

--- 初學/flask/app.py
+++ 初學/flask/app.py
@@ -3,7 +3,11 @@ app=Flask(__name__)    #__name__ 代表目前執行的模組
 
 @app.route("/")    #函式的裝飾(Decotator):以函式為基礎,提供附加功能
 def home():
-	return "Hello Flask"
+	return "Hello Flask in /"
+
+@app.route("/test")    #函式的裝飾(Decotator):以函式為基礎,提供附加功能
+def test():
+	return "This is test"
 
 if __name__ =="__main__":    #如果以主程式執行
 	app.run()    #立刻啟動伺服器
